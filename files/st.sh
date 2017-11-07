9068fd02a15a4c81bf0cfd6756702a3f9d2f90a8
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-07 15:43:08 +0000

    auxiliary files

---
+++ files/st.sh
@@ -0,0 +1,11 @@
+# 6-11-2017 MRC-Epid JHZ
+
+git add .gitignore
+git commit -m "Don't process these"
+git add files
+git commit -m "auxiliary files"
+git add pw-pipeline.sh
+git commit -m "main program"
+git add README.*
+git commit -m "README"
+git push
