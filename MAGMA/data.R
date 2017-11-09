7cb6cd71b30853947d2b332fe0ee2cd4e0d31509
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-09 09:59:39 +0000

    auxiliary directories

--- MAGMA/data.R
+++ MAGMA/data.R
@@ -5,5 +5,5 @@ source(sumstats)
 colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "P", "NOBS", "CHR", "BP")
 d <- d[c("SNP","CHR","BP","P","NOBS")]
 d <- subset(d, SNP!=".")
-write.table(d[c("SNP","CHR","BP")],file="MAGMA/magma.snploc",quote=FALSE,row.name=FALSE,col.names=FALSE,sep="\t")
-write.table(d[c("SNP","P","NOBS")],file="MAGMA/magma.pval",quote=FALSE,row.name=FALSE,sep="\t")
+write.table(d[c("SNP","CHR","BP")],file="magma.snploc",quote=FALSE,row.name=FALSE,col.names=FALSE,sep="\t")
+write.table(d[c("SNP","P","NOBS")],file="magma.pval",quote=FALSE,row.name=FALSE,sep="\t")
--- MAGMA/magma.sh
+++ MAGMA/magma.sh
@@ -1,3 +1,3 @@
 # Gene-set analysis, msigdb.v6.0.entrez.gmt
-magma --gene-results magma.genes.raw --set-annot $MSigDB/msigdb.v6.0.entrez.gmt self-contained --out magma
+magma --gene-results magma.genes.raw --set-annot $msigdb self-contained --out magma
 
