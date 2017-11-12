6e4fcccf09d05de48a7c7e9acf85566834a7ea58
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-12 15:05:27 +0000

    auxiliary files

---
+++ files/sumstats.R
@@ -0,0 +1,11 @@
+# 12-11-2017 MRC-Epid JHZ
+
+options(digits = 3, scipen=20)
+library(Rmpfr)
+sumstats <- Sys.getnev("sumstats")
+d <- read.table(sumstats,as.is=TRUE)
+colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "P", "N", "chr", "pos")
+d <- within(d, {
+  z_score <- b/se
+  p <- format(2*pnorm(mpfr(abs(z_score),100),lower.tail=FALSE))
+})
