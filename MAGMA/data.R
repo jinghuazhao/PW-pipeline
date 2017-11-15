791f57093c8560e067627ed742cfa149b4344984
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 14:55:19 +0000

    software-specific files

--- MAGMA/data.R
+++ MAGMA/data.R
@@ -1,6 +1,6 @@
-# 12-11-2017 MRC-Epid JHZ
+# 15-11-2017 MRC-Epid JHZ
 
-sumstats <- Sys.getnev("sumstats")
+sumstats <- Sys.getenv("sumstats")
 source(sumstats)
 colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "P", "NOBS", "CHR", "BP")
 d <- d[c("SNP","CHR","BP","P","NOBS")]
