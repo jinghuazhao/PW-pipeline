9cb0d075f138960de0233d9152f8bfdb2c566ca7
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 14:55:18 +0000

    auxiliary files

--- files/sumstats.R
+++ files/sumstats.R
@@ -5,7 +5,7 @@ options(digits = 3, scipen=20)
 SUMSTATS <- Sys.getenv("SUMSTATS")
 if(file.exists("sumstats.rda")) load("sumstats.rda") else {
   library(Rmpfr)
-  sumstats <- Sys.getnev(SUMSTATS)
+  sumstats <- Sys.getenv(SUMSTATS)
   d <- read.table(sumstats,as.is=TRUE)
   colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "P", "N", "chr", "pos")
   d <- within(d, {
