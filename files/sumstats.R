7a030574c79e99c6fca1e23137490c4b3fd177eb
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 15:53:31 +0000

    auxiliary files

--- files/sumstats.R
+++ files/sumstats.R
@@ -3,7 +3,7 @@
 
 options(digits = 3, scipen=20)
 
-args <- commandArgs(trailingOnly=FALSE)
+args <- commandArgs(trailingOnly=TRUE)
 sumstats <- args[1]
 if(!file.exists("sumstats.rda")) {
   library(Rmpfr)
