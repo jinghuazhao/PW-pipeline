4cbd39912684d188503ab24881580e11033e3d48
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 10:43:07 +0000

    software-specific files

--- MAGMA/magma.sh
+++ MAGMA/magma.sh
@@ -1,5 +1,5 @@
 # 15-11-2017 MRC-Epid JHZ
 
 # Gene-set analysis
-magma --gene-results magma.genes.raw --set-annot $MAGMA_DB self-contained --out magma
+magma --gene-results magma.genes.raw --set-annot $db self-contained --out magma
 
--- MAGMA/sets.R
+++ MAGMA/sets.R
@@ -1,5 +1,6 @@
-# 3-10-2017 MRC-Epid JHZ
+# 15-11-2017 MRC-Epid JHZ
 
+options(width=250)
 sets <- function(id)
 {
   sets.out <- read.table(paste0(id,".sets.out"), as.is=TRUE, skip=3, header=TRUE)
@@ -15,25 +16,8 @@ sets <- function(id)
   sets.out[ordered, keep_var]
 }
 
-options(width=250)
-magma <- sets("magma")
-ord1 <- with(magma,order(P))
-set1 <- magma[ord1,]
-c2 <- sets("c2")
-ordc2 <- with(c2,order(P))
-c2 <- c2[ordc2,]
-## add FULL_NAME
-M <- sets("MAGENTA_10")
-id <- read.table("MAGENTA_10.id",as.is=TRUE,col.names=c("FULL_NAME","pathway"),sep="\t",quote="")
-# library(readstata13)
-# id <- read.dta13("MAGENTA.dta")
-dim(id)
-MAGENTA <- merge(M,id,by="FULL_NAME",all=TRUE)
-MAGENTA <- within(MAGENTA,{FULL_NAME=substring(FULL_NAME,7)})
-dim(MAGENTA)
-ord2 <- with(MAGENTA,order(P))
-set2 <- MAGENTA[ord2,]
-depict <- sets("depict")
-ord3 <- with(depict,order(P))
-set3 <- depict[ord3,]
-save(set1,c2,set2,set3,file="MAGMA.rda")
+db <- Sys.getenv("db")
+magma <- sets(db)
+ord <- with(magma,order(P))
+set <- magma[ord,]
+save(set,file="MAGMA.rda")
