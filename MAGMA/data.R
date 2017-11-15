4583d9415888782b2ab80e2f8bc0743dd0538539
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 17:22:59 +0000

    software-specific files

--- MAGMA/data.R
+++ MAGMA/data.R
@@ -4,6 +4,7 @@ sumstats <- Sys.getenv("sumstats_rda")
 load(sumstats)
 colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "P", "NOBS", "CHR", "BP")
 d <- d[c("SNP","CHR","BP","P","NOBS")]
+d <- within(d, {NOBS <- as.integer(NOBS})
 d <- subset(d, SNP!=".")
 write.table(d[c("SNP","CHR","BP")],file="magma.snploc",quote=FALSE,row.name=FALSE,col.names=FALSE,sep="\t")
 write.table(d[c("SNP","P","NOBS")],file="magma.pval",quote=FALSE,row.name=FALSE,sep="\t")
