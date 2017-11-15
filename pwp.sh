5348f1cf49d3cb09537ba1004f4f8057d24eaa71
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 16:25:53 +0000

    main program

--- pwp.sh
+++ pwp.sh
@@ -27,7 +27,7 @@ export depict_db=$PASCAL/resources/genesets/depict_discretized_cutoff3.2.gmt
 
 ## indidivual analyses according to request
 
-R -q --no-save --slave --file=${PW_location}/files/sumstats.R --args ${PWD}/$1
+R -q --no-save --file=${PW_location}/files/sumstats.R --args ${PWD}/$1
 export sumstats=${PWD}/sumstats.rda
 
 if [ $_db == "magenta" ]; then
