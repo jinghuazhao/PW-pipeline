f4e3c3f48acd60f2152afaf8d7a7e1a4903a29d4
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 14:13:09 +0000

    main program

--- pwp.sh
+++ pwp.sh
@@ -19,22 +19,19 @@ export magma=1
 export pascal=1
 export depict=1
 
-### database flags and names
+### database flags (magenta, c2, msigdb, depict) and names
 
-export magenta_db=1
-export c2=0
-export msigdb=0
-export depict_db=1
-export c2.db=$MSigDB/msigdb_v6.0_GMTs/c2.all.v6.0.entrez.gmt
-export msigdb.db=$MSigDB/msigdb_v6.0_GMTs/msigdb.v6.0.entrez.gmt
-export depict.db=$PASCAL/resources/genesets/depict_discretized_cutoff3.2.gmt
+export _db=c2
+export c2_db=$MSigDB/msigdb_v6.0_GMTs/c2.all.v6.0.entrez.gmt
+export msigdb_db=$MSigDB/msigdb_v6.0_GMTs/msigdb.v6.0.entrez.gmt
+export depict_db=$PASCAL/resources/genesets/depict_discretized_cutoff3.2.gmt
 
 ## indidivual analyses according to request
 
 export SUMSTATS=$1
 export sumstats=${PW_location}/files/sumstats.R
 
-if [ $magenta_db -eq 1 ]; then
+if [ $_db == "magenta" ]; then
    if [ ! -d "MAGENTA" ]; then
       mkdir MAGENTA
    fi
@@ -54,17 +51,17 @@ if [ $magenta -eq 1 ]; then
    for f in $(ls $MAGENTA); do ln -sf $f; done
    for f in ($ls $PW_location/MAGENTA); do ln -sf $f; done
    R -q --no-save < data.R > data.log
-   if [ $magenta_db -eq 1 ]; then
+   if [ $_db -eq "magenta" ]; then
       export db=magenta.db
-   elif [ $c2 -eq 1 ]; then
+   elif [ $_db == "c2" ]; then
       export db=c2.db
-      awk '{$2=$1; $1="c2"};1' FS="\t" OFS="\t" ${c2.db} > c2.db
-   elif [ $msigdb -eq 1 ]; then
+      awk '{$2=$1; $1="c2"};1' FS="\t" OFS="\t" ${c2_db} > c2.db
+   elif [ $_db == "msigdb" ]; then
       export db=msigdb.db
-      awk '{$2=$1; $1="msigdb"};1' FS="\t" OFS="\t" ${msigdb.db} > msigdb.db
+      awk '{$2=$1; $1="msigdb"};1' FS="\t" OFS="\t" ${msigdb_db} > msigdb.db
    else
       export db=depict.db
-      awk '{FS=OFS="\t";$2=$1;$1="depict";print}' ${depict.db} > depict.db
+      awk '{FS=OFS="\t";$2=$1;$1="depict";print}' ${depict_db} > depict.db
    fi
    sed -i 's|magenta.db|'"$db"'|g' magenta.m
    qsub -V -sync y ${PW_location}/MAGENTA/magenta.sh
@@ -85,15 +82,15 @@ if [ $magma -eq 1 ]; then
    magma --annotate window=50,50 --snp-loc magma.snploc --gene-loc $MAGMA/NCBI37.3.gene.loc --out magma
    # Gene analysis - SNP p-values
    magma --bfile $MAGMA/g1000_eur --pval magma.pval ncol=NOBS --gene-annot magma.genes.annot --out magma
-   if [ $magenta_db -eq 1 ]; then
+   if [ $_db == "magenta" ]; then
       awk -vFS="\t" '{$1=$2;$2=""};1' $magenta.db | awk '{$2=$2};1'> magenta.db
       export db=magenta.db
-   elif [ $msigdb_c2 -eq 1 ]; then
-      export db=${c2.db}
-   elif [ $msigbdb -eq 1 ]; then
-      export db=${msigdb.db}
+   elif [ $_db == "c2" ]; then
+      export db=${c2_db}
+   elif [ $_db == "msigdb" ]; then
+      export db=${msigdb_db}
    else
-      export db=${depict.db}
+      export db=${depict_db}
    fi
    qsub -V -sync y ${PW_location}/MAGMA/magma.sh
    R -q --no-save < collect.R > collect.log
@@ -109,14 +106,14 @@ if [ $pascal -eq 1 ]; then
    cp $PW_location/PASCAL/* .
    R -q --no-save < data.R > data.log
    sed -i 's|OUTPUTDIRECTORY|'"$PWD"'|g; s|PASCAL_location|'"$PASCAL"'|g' settings.txt
-   if [ $magenta_db -eq 1 ]; then
-      sed -i 's|GENESETFILE|'"${magenta.db}"'|g' settings.txt
-   elif [ $msigdb_c2 -eq 1 ]; then
-      sed -i 's|GENESETFILE|'"${c2.db}"'|g' settings.txt
-   elif [ $msigdb -eq 1 ]; then
-      sed -i 's|GENESETFILE|'"${msigdb.db}"'|g' settings.txt
+   if [ $_db == "magenta" ]; then
+      sed -i 's|GENESETFILE|'"${magenta_db}"'|g' settings.txt
+   elif [ $_db == "c2" ]; then
+      sed -i 's|GENESETFILE|'"${c2_db}"'|g' settings.txt
+   elif [ $_db == "msigdb" ]; then
+      sed -i 's|GENESETFILE|'"${msigdb_db}"'|g' settings.txt
    else
-      sed -i 's|GENESETFILE|'"${depict.db}"'|g' settings.txt
+      sed -i 's|GENESETFILE|'"${depict_db}"'|g' settings.txt
    fi
    qsub -V -sync y ${PW_location}/PASCAL/pascal.sh
    R -q --no-save < collect.R > collect.log
@@ -132,7 +129,7 @@ if [ $depict -eq 1 ]; then
    R -q --no-save < ${PW_location}/DEPICT/data.R > data.log
    cp $PW_location/DEPICT/* .
    sed -i 's|ANALYSIS_PATH|'"$PWD"'|g; s|PLINK_EXECUTABLE|'"$PLINK_EXECUTABLE"'|g' depict.cfg
-   if [ $depict_db -eq 1 ]; then
+   if [ $_db == "depict" ]; then
       sed -i 's|RECONSTITUTED_GENESETS_FILE|data/reconstituted_genesets/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary|g' depict.cfg
    else
       sed -i 's|RECONSTITUTED_GENESETS_FILE|data/reconstituted_genesets/reconstituted_genesets_150901.binary|g' depict.cfg
@@ -143,7 +140,7 @@ fi
 
 ## collection into Excel workbook(s)
 
-if [ $magenta -eq 1 ] && [ $magma -eq 1 ] && [ $pascal -eq 1 ] && [ $depict -eq 1 ] && [ $depict_db -eq 1]; then
+if [ $magenta -eq 1 ] && [ $magma -eq 1 ] && [ $pascal -eq 1 ] && [ $depict -eq 1 ] && [ $_db == "depict" ]; then
     R -q --no-save < ${PW_location}/files/mmpd.R > mmpd.log
 elif [ $magenta -eq 1 ] && [ $magma -eq 1 ] && [ $pascal -eq 1 ] && [ $depict -eq 1 ]; then
     R -q --no-save < ${PW_location}/files/mmp.R > mmp.log
