68add0fd08e1a1094667c3b1ebe495dffb9ecc24
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-15 09:29:16 +0000

    Don't process these

--- PASCAL/magenta_10.sh
+++
@@ -1,3 +0,0 @@
-# 14-11-2017 MRC-Epid JHZ
-
-/genetics/bin/Pascal --set=magenta_10.txt --pval=vegas2v2 --runpathway=on --mafcutoff=0.05
--- PASCAL/msigdb.sh
+++
@@ -1,3 +0,0 @@
-# 14-11-2017 MRC-Epid JHZ
-
-/genetics/bin/Pascal --set=msigdb.txt --pval=vegas2v2 --runpathway=on --mafcutoff=0.05
