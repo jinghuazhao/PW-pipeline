9a028bf7efc40313402c107f330bd2b397c09318
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-16 09:25:22 +0000

    software-specific files

---
+++ DEPICT/collect.R
@@ -0,0 +1,28 @@
+# 16-11-2017 MRC-Epid JHZ
+
+options(digits=3, scipen=20, width=200)
+library(openxlsx)
+xlsx <- "depict.xlsx"
+unlink(xlsx, recursive = FALSE, force = FALSE)
+wb <- createWorkbook(xlsx)
+
+for (tbl in c("_genesetenrichment.txt", "_geneprioritization.txt", "_loci.txt", "_tissueenrichment.txt",".clumped", "_depict.tab"))
+{
+  file <- paste0("depict",tbl)
+  sep <- ifelse(tbl==".clumped","", "\t")
+  assign(file,read.table(file,as.is=TRUE,header=TRUE,sep=sep,quote=""))
+  addWorksheet(wb, paste0("DEPICT",tbl))
+  dat <- get(file)
+  if(tbl=="_genesetenrichment.txt") dat <- within(dat,dat[order(Nominal.P.value),])
+  writeDataTable(wb,paste0("DEPICT",tbl),dat)
+}
+for(s in c("cells","multiplot","system", "tissues"))
+{
+  i <- paste0("DEPICT_",s)
+  addWorksheet(wb, i)
+  insertImage(wb, i, paste0("tissue_plot_depict_genenetwork_",s,".png"), width=12, height=6)
+}
+cat("See\nhttps://github.com/perslab/depict/wiki/DEPICT-result-files-format\n for header information\n")
+saveWorkbook(wb, file=xlsx, overwrite=TRUE)
+
+
