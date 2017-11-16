76a3f1d1f9d85c9bf472c5682d1b713d02d1ae01
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-16 08:51:10 +0000

    software-specific files

--- MAGENTA/data.R
+++ MAGENTA/data.R
@@ -1,6 +1,6 @@
-# 15-11-2017 MRC-Epid JHZ
+# 16-11-2017 MRC-Epid JHZ
 
 sumstats <- Sys.getenv("sumstats_rda")
 load(sumstats)
-write.table(d[,c("chr","pos","z_score","p")],file="magenta",
-            quote=FALSE,col.names=FALSE,row.name=FALSE,sep="\t")
+d <- d[c("chr","pos","z_score","p")]
+write.table(d,file="magenta",quote=FALSE,col.names=FALSE,row.name=FALSE,sep="\t")
