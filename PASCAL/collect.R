036c7fb1b295a4dd2abd3137230aaa271abaf0c1
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-18 21:18:04 +0000

    software-specific files

--- PASCAL/collect.R
+++ PASCAL/collect.R
@@ -5,13 +5,10 @@ gsps <- function(f,db="MAGENTA",method="sum")
   gs <- read.table(paste0(f,".",method,".genescores.txt"),as.is=TRUE,header=TRUE,sep="\t")
   ord <- with(gs,order(pvalue))
   gs <- gs[ord,]
-  fg <- read.table(paste0(f,".",method,".fusion.genescores.txt"),as.is=TRUE,header=TRUE,sep="\t")
-  ord <- with(fg,order(pvalue))
-  fg <- fg[ord,]
   ps <- read.table(paste0(f,".PathwaySet--",db,"--",method,".txt"),as.is=TRUE,header=TRUE,sep="\t")
   ord <- with(ps,order(chi2Pvalue))
   ps <- ps[ord,]
-  save(gs,fg,ps,file="PASCAL.rda")
+  save(gs,ps,file="PASCAL.rda")
 }
 
 db <- Sys.getenv("db")
