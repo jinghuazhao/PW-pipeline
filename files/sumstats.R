da04557ddc4c7e60417f84591561ffd1b54b1c7f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-25 17:01:01 +0000

    auxiliary files

--- files/sumstats.R
+++ files/sumstats.R
@@ -3,13 +3,21 @@
 options(digits = 3, scipen=20)
 
 sumstats <- Sys.getenv("sumstats")
+mp <- Sys.getenv("mp")
 if (!file.exists("sumstats.rda")) {
-  library(Rmpfr)
   d <- read.table(sumstats,as.is=TRUE)
   colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "p", "N", "chr", "pos")
   d <- within(d, {
-    z_score <- b/se
-    P <- format(2*pnorm(mpfr(abs(z_score),100),lower.tail=FALSE))
+    if(mp=="1")
+    {
+      library(Rmpfr)
+      z_score <- b/se
+      P <- format(2*pnorm(mpfr(abs(z_score),100),lower.tail=FALSE))
+      logP <- as.numeric(-log10(mpfr(P,100)))
+    } else {
+      P <- p
+      logP <- log10(P)
+    }
   })
   save(d,file="sumstats.rda")
 }
@@ -17,4 +25,3 @@ if (!file.exists("sumstats.rda")) {
 #!/bin/env Rscript --vanilla -q
 #args <- commandArgs(trailingOnly=TRUE)
 #sumstats <- args[1]
-
