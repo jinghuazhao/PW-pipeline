82b5fa337310a80610c9d235692e4ce04fab9e95
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-25 17:07:46 +0000

    software-specific files

--- DEPICT/data.R
+++ DEPICT/data.R
@@ -2,11 +2,15 @@
 
 sumstats <- Sys.getenv("sumstats_rda")
 load(sumstats)
+mp <- Sys.getenv("mp")
 d <- within(d, {
   Chr <- chr
   Pos <- pos
   Marker <- sprintf("%s:%d",Chr,Pos)
 })[c("SNP","Chr","Pos","P","logP","Marker")]
-gwas_threshold <- as.numeric(-log10(mpfr(5e-8,100)))
+if(mp=="1") {
+  library(Rmpfr)
+  gwas_threshold <- as.numeric(-log10(mpfr(5e-8,100)))
+}
 z <- gzfile("depict.txt.gz")
 write.table(subset(d,logP>=gwas_threshold),file=z,quote=FALSE,row.name=FALSE,sep="\t")
