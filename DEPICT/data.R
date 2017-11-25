91d6b9e7f393e027511ef9bce47edced1812154f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-25 17:01:02 +0000

    software-specific files

--- DEPICT/data.R
+++ DEPICT/data.R
@@ -1,12 +1,10 @@
-# 16-11-2017 MRC-Epid JHZ
+# 26-11-2017 MRC-Epid JHZ
 
 sumstats <- Sys.getenv("sumstats_rda")
 load(sumstats)
 d <- within(d, {
-  library(Rmpfr)
   Chr <- chr
   Pos <- pos
-  logP <- as.numeric(-log10(mpfr(P,100)))
   Marker <- sprintf("%s:%d",Chr,Pos)
 })[c("SNP","Chr","Pos","P","logP","Marker")]
 gwas_threshold <- as.numeric(-log10(mpfr(5e-8,100)))
