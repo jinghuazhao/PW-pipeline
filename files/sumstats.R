c25e1ab8f34c8a46bba0af013c143d2aba87dbc2
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-28 10:10:02 +0000

    auxiliary files

--- files/sumstats.R
+++ files/sumstats.R
@@ -1,4 +1,4 @@
-# 25-11-2017 MRC-Epid JHZ
+# 28-11-2017 MRC-Epid JHZ
 
 options(digits = 3, scipen=20)
 
@@ -8,10 +8,10 @@ if (!file.exists("sumstats.rda")) {
   d <- read.table(sumstats,as.is=TRUE)
   colnames(d) <- c("SNP", "A1", "A2", "AF1", "b", "se", "p", "N", "chr", "pos")
   d <- within(d, {
+    z_score <- b/se
     if(mp=="1")
     {
       library(Rmpfr)
-      z_score <- b/se
       P <- format(2*pnorm(mpfr(abs(z_score),100),lower.tail=FALSE))
       logP <- as.numeric(-log10(mpfr(P,100)))
     } else {
