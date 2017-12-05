ac515de67410305e7c62cc44542d11efe4044a1e
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-05 14:28:25 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -9,9 +9,12 @@ gunzip -c ${BP} | \
 awk -vFS="\t" -vOFS="\t" -f xpose.awk | \
 grep -f ${_db}.colnames | \
 awk -f xpose.awk > ${_db}.network
+zgrep -n -T -f ${_db}.colnames ${columns} | \
+cut -f1 > ${_db}.colid
 
 R --no-save <<END
   pdf("network.pdf")
+  set.seed(31415625)
   db <- Sys.getenv("_db")
   nw <- read.table(paste0(db,".network"),as.is=TRUE,header=TRUE)
   Raw <- nw[,-1]
@@ -26,27 +29,25 @@ R --no-save <<END
   summary(g)
   plot(g)
   require(factoextra)
-  fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-  cl <- kmeans(distance, 9, nstart = 20)
+  fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
+  cl <- kmeans(distance,9,nstart=20)
   fviz_cluster(cl,data=distance)
   tRaw <- t(Raw)
-  set.seed(31415625)
   fviz_nbclust(tRaw, kmeans, method="gap_stat", nboot=5)
   gap_stat <- clusGap(tRaw, FUN=kmeans, nstart=25, K.max=10, B=5)
   fviz_gap_stat(gap_stat)
   require(NbClust)
-  nb <- NbClust(tRaw, distance="euclidean", min.nc=2, max.nc=10, method="kmeans", index="all")
+  nb <- NbClust(tRaw,distance="euclidean",min.nc=2,max.nc=10,method="kmeans",index="all")
 # uninformative
 # require(spatstat)
-# plot(im(corRaw[nrow(corRaw):1,]), main="Correlation Matrix Map")
+# plot(im(corRaw[nrow(corRaw):1,]),main="Correlation Matrix Map")
 # too slow
 # require(pvclust)
-# cl.bootstrap <- pvclust(Raw, nboot=1000,method.dist="correlation")
+# cl.bootstrap <- pvclust(Raw,nboot=1000,method.dist="correlation")
 # plot(cl.bootstrap)
 # pvrect(cl.bootstrap)
   dev.off()
 END
-zgrep -n -T -f depict_discretized_cutoff3.2.colnames ${columns} | cut -f1 > depict_discretized_cutoff3.2.colid
 
 # http://research.stowers.org/mcm/efg/R/Visualization/cor-cluster/index.htm
 # http://www.sthda.com/english/wiki/print.php?id=239
