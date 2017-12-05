3d94cb56cbf2fcf6f105679969fbe1902b1015fc
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-05 11:14:42 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -1,4 +1,4 @@
-# 4-12-2017 MRC-Epid JHZ
+# 5-12-2017 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
@@ -14,20 +14,32 @@ R --no-save <<END
   nw <- read.table("depict_discretized_cutoff3.2.network",as.is=TRUE,header=TRUE)
   Raw <- nw[,-1]
   corRaw <- cor(Raw)
-  require(spatstat)
-  plot(im(corRaw[nrow(corRaw):1,]), main="Correlation Matrix Map")
   dissimilarity <- 1-abs(cor(corRaw))
   distance <- as.dist(dissimilarity)
-  require(cluster)
-  plot(pam(distance,5))
-# require(pvclust)
-# cluster.bootstrap <- pvclust(Raw, nboot=1000,method.dist="correlation")
-# plot(cluster.bootstrap)
-# pvrect(cluster.bootstrap)
-  cl <- kmeans(dissimilarity, 5, nstart = 25)
   require(factoextra)
-  fviz_dist(as.dist(dissimilarity), gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+  fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+  tRaw <- t(Raw)
+  set.seed(31415625)
+  fviz_nbclust(tRaw, kmeans, method = "gap_stat")
+  gap_stat <- clusGap(tRaw, FUN = kmeans, nstart = 25, K.max = 10, B = 50)
+  fviz_gap_stat(gap_stat)
+  cl <- kmeans(distance, 4, nstart = 20)
+  fviz_cluster(cl,data=dissimilarity)
+# missing basics?
+  require(cluster)
+  plot(pam(distance,4))
+# not very informative
+# require(spatstat)
+# plot(im(corRaw[nrow(corRaw):1,]), main="Correlation Matrix Map")
+# too slow p-value?
+# require(pvclust)
+# cl.bootstrap <- pvclust(Raw, nboot=1000,method.dist="correlation")
+# plot(cl.bootstrap)
+# pvrect(cl.bootstrap)
+  require(NbClust)
+nb <- NbClust(tRaw, distance = "euclidean", min.nc = 2, max.nc = 10, method = "complete", index ="all")
 END
 zgrep -n -T -f depict_discretized_cutoff3.2.colnames ${columns} | cut -f1 > depict_discretized_cutoff3.2.colid
 
 # http://research.stowers.org/mcm/efg/R/Visualization/cor-cluster/index.htm
+# http://www.sthda.com/english/wiki/print.php?id=239
