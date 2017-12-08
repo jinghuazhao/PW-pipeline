620d2f8eeea67fe015b280a65cc55211c5d4dc9e
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-08 10:57:20 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -1,31 +1,44 @@
-# 7-12-2017 MRC-Epid JHZ
+# 8-12-2017 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
 export columns=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.columns.txt
 export rows=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.rows.txt
 
+# An DEPICT example, where 1418 pathways have FDR<0.05
+export _db=depict
+awk 'NR>1{print $1}' ${_db}_genesetenrichment.txt | \
+head -1419 > ${_db}.colnames
 gunzip -c ${BP} | \
 awk -vFS="\t" -vOFS="\t" -f xpose.awk | \
-grep -f ${_db}.colnames | \
+grep -w -f ${_db}.colnames | \
 awk -f xpose.awk > ${_db}.network
-zgrep -n -T -f ${_db}.colnames ${columns} | \
+zgrep -n -T -w -f ${_db}.colnames ${columns} | \
 cut -f1 > ${_db}.colid
 
 R --no-save <<END
+  require(apcluster)
   require(cluster)
-  require(network)
   require(factoextra)
-  require(NbClust)
   require(graph)
   require(igraph)
+  require(network)
+  require(reshape)
   require(RCytoscape)
+  require(NbClust)
   set.seed(31415625)
   db <- Sys.getenv("_db")
   nw <- read.table(paste0(db,".network"),as.is=TRUE,header=TRUE)
   Raw <- nw[,-1]
   corRaw <- cor(Raw)
   colnames(corRaw) <- rownames(corRaw) <- names(Raw)
+  apres <- apcluster(corSimMat,Raw,details=TRUE)
+  show(apres)
+  pdf("network.pdf")
+  plot(apres,Raw)
+  r <- melt(corRaw)
+  el <- cbind(r[1],r[3],r[2])
+  write.table(el,file="network.sif",as.is=TRUE,quote=FALSE,rownames=FALSE)
   distance <- as.dist(1-abs(corRaw))
   m <- (abs(corRaw)>0.7)+0
   diag(m) <- 0
@@ -33,7 +46,6 @@ R --no-save <<END
   get.edgelist(g)
   write_graph(g,"network.el","edgelist")
   g <- network(m, directed=FALSE)
-  pdf("network.pdf")
   plot(pam(distance,9))
   plot(g)
   fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
