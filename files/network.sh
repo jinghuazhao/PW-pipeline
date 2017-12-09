cdb439bb9ad77b97a463a1bf87117bf879bb2707
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-09 09:45:03 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -1,4 +1,4 @@
-# 8-12-2017 MRC-Epid JHZ
+# 9-12-2017 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
@@ -38,22 +38,24 @@ R --no-save <<END
   db <- Sys.getenv("_db")
   nw <- read.table(paste0(db,".network"),as.is=TRUE,header=TRUE)
   Raw <- nw[,-1]
+  tRaw <- t(Raw)
   z <- gzfile("id_descrip.txt.gz")
   id_descrip <- read.table(z,sep="\t",header=TRUE,quote="")
   close(z)
-  descrip <- id_descrip[with(id_descrip,Original.gene.set.ID)%in%names(Raw),"Original.gene.set.description"]
+  descrip <- id_descrip[names(Raw)%in%with(id_descrip,Original.gene.set.ID),"Original.gene.set.description"]
   names(Raw) <- descrip
-  apres <- apcluster(corSimMat,t(Raw),details=TRUE)
+  apres <- apcluster(corSimMat,tRaw,details=TRUE)
   show(apres)
   pdf("network.pdf")
-  plot(apres,Raw)
+  plot(apres,tRaw)
   heatmap(apres)
   corRaw <- cor(Raw)
   colnames(corRaw) <- rownames(corRaw) <- names(Raw)
   r <- melt(corRaw)
   el <- cbind(r[1],r[3],r[2])
-  write.table(el,file="network.sif",as.is=TRUE,quote=FALSE,rownames=FALSE)
+  write.table(el,file="network.sif",quote=FALSE,row.names=FALSE)
   distance <- as.dist(1-abs(corRaw))
+  fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
   m <- (abs(corRaw)>0.7)+0
   diag(m) <- 0
   g <- graph.adjacency(m)
@@ -62,17 +64,17 @@ R --no-save <<END
   g <- network(m, directed=FALSE)
   plot(pam(distance,9))
   plot(g)
-  fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
-  cl <- kmeans(distance,9,nstart=20)
+  cl <- kmeans(distance,5,nstart=20)
   fviz_cluster(cl,data=distance)
-  tRaw <- t(Raw)
+  gmat <- new("graphAM", adjMat=m, edgemode='directed')
+  glist <- as(gmat, 'graphNEL')
+  plot(glist)
+  dev.off()
+  pdf("nb.pdf")
   gap_stat <- clusGap(tRaw, FUN=kmeans, nstart=25, K.max=10, B=5)
   fviz_gap_stat(gap_stat)
   fviz_nbclust(tRaw, kmeans, method="gap_stat", nboot=5)
   nb <- NbClust(tRaw,distance="euclidean",min.nc=2,max.nc=10,method="kmeans",index="all")
-  gmat <- new("graphAM", adjMat=m, edgemode='directed')
-  glist <- as(gmat, 'graphNEL')
-  plot(glist)
   dev.off()
 # uninformative
 # require(spatstat)
