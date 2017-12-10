808341d4ca7121511bad094e2b4cd81c05631b79
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-10 11:07:39 +0000

    auxiliary files

---
+++ files/network.R
@@ -0,0 +1,61 @@
+# 10-12-2017 MRC-Epid JHZ
+
+db <- Sys.getenv("_db")
+options(width=200)
+set.seed(31415625)
+
+nw <- read.table(paste0(db,".network"),as.is=TRUE,header=TRUE,quote="")
+Raw <- nw[,-1]
+tRaw <- t(Raw)
+corRaw <- cor(Raw)
+colnames(corRaw) <- rownames(corRaw) <- names(Raw)
+require(reshape)
+r <- melt(corRaw)
+e <- cbind(r[1],r[3],r[2])
+write.table(e,file="network.sif",col.names=FALSE,row.names=FALSE,quote=FALSE)
+m <- (abs(corRaw)>0.7)+0
+diag(m) <- 0
+z <- gzfile("id_descrip.txt.gz")
+id_descrip <- read.table(z,sep="\t",as.is=TRUE,header=TRUE,quote="")
+close(z)
+descrip <- id_descrip[with(id_descrip,gsub(":",".",Original.gene.set.ID))%in%names(Raw),"Original.gene.set.description"]
+names(Raw) <- gsub(" ",".",descrip)
+
+require(igraph)
+g <- graph.adjacency(m)
+write_graph(g,"network.el","edgelist")
+require(apcluster)
+apres <- apcluster(corSimMat,tRaw,details=TRUE)
+show(apres)
+pdf("network.pdf")
+heatmap(apres)
+require(network)
+distance <- as.dist(1-abs(corRaw))
+g <- network(m, directed=FALSE)
+plot(pam(distance,5))
+plot(g)
+require(cluster)
+cl <- kmeans(distance,5,nstart=20)
+require(factoextra)
+fviz_cluster(cl,data=distance)
+## change as appropriate
+if(0) { 
+  require(graph)
+  gmat <- new("graphAM", adjMat=m, edgemode='undirected')
+  glist <- as(gmat, 'graphNEL')
+  plot(glist)
+  gap_stat <- clusGap(tRaw, FUN=kmeans, nstart=25, K.max=10, B=5)
+  fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
+  fviz_gap_stat(gap_stat)
+  fviz_nbclust(tRaw, kmeans, method="gap_stat", nboot=5)
+  require(NbClust)
+  nb <- NbClust(tRaw,distance="euclidean",min.nc=2,max.nc=10,method="kmeans",index="all")
+  require(RCytoscape)
+  require(spatstat)
+  plot(im(corRaw[nrow(corRaw):1,]),main="Correlation Matrix Map")
+  require(pvclust)
+  cl.bootstrap <- pvclust(Raw,nboot=1000,method.dist="correlation")
+  plot(cl.bootstrap)
+  pvrect(cl.bootstrap)
+}
+dev.off()
--- files/network.sh
+++ files/network.sh
@@ -1,4 +1,4 @@
-# 9-12-2017 MRC-Epid JHZ
+# 10-12-2017 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
@@ -12,6 +12,8 @@ head -1418 > ${_db}.colnames
 zgrep -n -T -x -f ${_db}.colnames ${columns} | \
 cut -f1 > ${_db}.colid
 fn=$(cat ${_db}.colid)
+
+## fast cut!
 echo $fn > ${_db}.cat
 fields=$(sed 's/ /,/g' ${_db}.cat)
 gunzip -c ${BP} | \
@@ -20,75 +22,11 @@ gunzip -c ${BP} | \
 cut -f1 --complement | \
 cut -f$fields | \
 paste ${_db}.rownames - > ${_db}.network
-gunzip -c id_descrip.txt.gz  | \
-cut -f2 > descrip
-fd=$(cat descrip)
-echo $fd | \
-cut -d' ' -f$fields --output-delimiter=$'\t' > ${_db}.descrip
 
-## v slow!
+## vv slow!
 gunzip -c ${BP} | awk -vFS="\t" -vOFS="\t" -f xpose.awk | grep -x -f ${_db}.colnames | awk -f xpose.awk > ${_db}.network
 
-R --no-save <<END
-  require(apcluster)
-  require(cluster)
-  require(factoextra)
-  require(graph)
-  require(igraph)
-  require(network)
-  require(reshape)
-  require(RCytoscape)
-  require(NbClust)
-  set.seed(31415625)
-  db <- Sys.getenv("_db")
-  nw <- read.table(paste0(db,".network"),as.is=TRUE,header=TRUE,quote="")
-  Raw <- nw[,-1]
-  tRaw <- t(Raw)
-  corRaw <- cor(Raw)
-  colnames(corRaw) <- rownames(corRaw) <- names(Raw)
-  r <- melt(corRaw)
-  e <- cbind(r[1],r[3],r[2])
-  write.table(e,file="network.sif",col.names=FALSE,row.names=FALSE,quote=FALSE)
-  m <- (abs(corRaw)>0.7)+0
-  diag(m) <- 0
-  g <- graph.adjacency(m)
-  write_graph(g,"network.el","edgelist")
-  z <- gzfile("id_descrip.txt.gz")
-  id_descrip <- read.table(z,sep="\t",as.is=TRUE,header=TRUE,quote="")
-  descrip <- id_descrip[names(Raw)%in%with(id_descrip,Original.gene.set.ID),"Original.gene.set.description"]
-  names(Raw) <- descrip
-  apres <- apcluster(corSimMat,tRaw,details=TRUE)
-  show(apres)
-  pdf("network.pdf")
-  plot(apres,tRaw)
-  heatmap(apres)
-  distance <- as.dist(1-abs(corRaw))
-  fviz_dist(distance,gradient=list(low="#00AFBB",mid="white",high="#FC4E07"))
-  g <- network(m, directed=FALSE)
-  plot(pam(distance,9))
-  plot(g)
-  cl <- kmeans(distance,5,nstart=20)
-  fviz_cluster(cl,data=distance)
-  gmat <- new("graphAM", adjMat=m, edgemode='directed')
-  glist <- as(gmat, 'graphNEL')
-  plot(glist)
-  dev.off()
-  pdf("nb.pdf")
-  gap_stat <- clusGap(tRaw, FUN=kmeans, nstart=25, K.max=10, B=5)
-  fviz_gap_stat(gap_stat)
-  fviz_nbclust(tRaw, kmeans, method="gap_stat", nboot=5)
-  nb <- NbClust(tRaw,distance="euclidean",min.nc=2,max.nc=10,method="kmeans",index="all")
-  dev.off()
-# uninformative
-# require(spatstat)
-# plot(im(corRaw[nrow(corRaw):1,]),main="Correlation Matrix Map")
-# too slow
-# require(pvclust)
-# cl.bootstrap <- pvclust(Raw,nboot=1000,method.dist="correlation")
-# plot(cl.bootstrap)
-# pvrect(cl.bootstrap)
-  close(z)
-END
+R --no-save -q < nework.R > network.log
 
 # http://research.stowers.org/mcm/efg/R/Visualization/cor-cluster/index.htm
 # http://www.sthda.com/english/wiki/print.php?id=239
