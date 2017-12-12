ccc65c14d3a2c30efcc72bdddc265d35edfce5a3
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-12 15:30:30 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -1,17 +1,28 @@
-# 10-12-2017 MRC-Epid JHZ
+# 12-12-2017 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
 export columns=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.columns.txt
 export rows=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.rows.txt
 
-# DEPICT result with FDR<0.05 for 1418 pathways
-export _db=depict
-export N=1418
-
 ## fast cut!
-awk 'NR>1{print $1}' ${_db}_genesetenrichment.txt | \
-head -$N > ${_db}.colnames
+case $1 in
+magenta)
+  echo magenta network analysis
+  export N=$(awk '(NR==1||$11<0.05)' ${_db}.dat|awk 'END{print NR}')
+  awk 'NR>1{print $2}' ${_db}.dat | \
+  head -$N > ${_db}.colnames
+  ;;
+depict)
+  echo depict network analysis
+  export N=$(awk -vFS="\t" '($4<0.05||$4=="<0.01"||$4=="<0.05")' ${_db}_genesetenrichment.txt|awk 'END{print NR}')
+  awk 'NR>1{print $1}' ${_db}_genesetenrichment.txt | \
+  head -$N > ${_db}.colnames
+  ;;
+*)
+  echo not implemented
+esac
+
 zgrep -n -T -x -f ${_db}.colnames ${columns} | \
 cut -f1 > ${_db}.colid
 fn=$(cat ${_db}.colid)
@@ -27,7 +38,7 @@ paste ${_db}.rownames - > ${_db}.network
 ## vv slow!
 gunzip -c ${BP} | awk -vFS="\t" -vOFS="\t" -f xpose.awk | grep -x -f ${_db}.colnames | awk -f xpose.awk > ${_db}.network
 
-R --no-save -q < network.R > network.log
+R --no-save -q < $PW_location/files/network.R > network.log
 
 # http://research.stowers.org/mcm/efg/R/Visualization/cor-cluster/index.htm
 # http://www.sthda.com/english/wiki/print.php?id=239
