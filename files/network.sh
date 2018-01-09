5865712c0074c9c797eb51b303bed84ef0c6437f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-09 17:13:32 +0000

    auxiliary files

--- files/network.sh
+++ files/network.sh
@@ -1,4 +1,4 @@
-# 31-12-2017 MRC-Epid JHZ
+# 9-1-2018 MRC-Epid JHZ
 
 export prefix=/genetics/bin/DEPICT/data/reconstituted_genesets
 export BP=${prefix}/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.txt.gz
@@ -10,46 +10,46 @@ export software=$1
 case $1 in
 magenta)
   echo MAGENTA network analysis
-  export N=$(awk '(NR==1||$11<0.05)' ${_db}.dat|awk 'END{print NR}')
-  awk 'NR>1{print $2}' ${_db}.dat | \
-  head -$N > ${_db}.colnames
+  export N=$(awk '(NR==1||$11<0.05)' ${db}.dat|awk 'END{print NR}')
+  awk 'NR>1{print $2}' ${db}.dat | \
+  head -$N > ${db}.colnames
   ;;
 magma)
   echo MAGMA network analysis
-  export N=$(awk 'NR>7&&$6<0.05' ${_db}.sets.out|awk 'END{print NR}')
-  awk 'NR>7{print $6}' ${_db}.sets.out | \
-  head -$N > ${_db}.colnames
+  export N=$(awk 'NR>7&&$6<0.05' ${db}.sets.out|awk 'END{print NR}')
+  awk 'NR>7{print $6}' ${db}.sets.out | \
+  head -$N > ${db}.colnames
   ;;
 pascal)
   echo PASCAL network analysis
-  export N=$(awk 'NR==1||$2<0.05' vegas2v2.PathwaySet--${_db}--sum.txt|awk 'END{print NR}')
-  awk 'NR>1{print $1}' vegas2v2.PathwaySet--${_db}--sum.txt | \
-  head -$N > ${_db}.colnames
+  export N=$(awk 'NR==1||$2<0.05' vegas2v2.PathwaySet--${db}--sum.txt|awk 'END{print NR}')
+  awk 'NR>1{print $1}' vegas2v2.PathwaySet--${db}--sum.txt | \
+  head -$N > ${db}.colnames
   ;;
 depict)
   echo depict network analysis
-  export N=$(awk -vFS="\t" '($4<0.05||$4=="<0.01"||$4=="<0.05")' ${_db}_genesetenrichment.txt|awk 'END{print NR}')
-  awk 'NR>1{print $1}' ${_db}_genesetenrichment.txt | \
-  head -$N > ${_db}.colnames
+  export N=$(awk -vFS="\t" '($4<0.05||$4=="<0.01"||$4=="<0.05")' ${db}_genesetenrichment.txt|awk 'END{print NR}')
+  awk 'NR>1{print $1}' ${db}_genesetenrichment.txt | \
+  head -$N > ${db}.colnames
   ;;
 *)
   echo not implemented
 esac
 
-zgrep -n -T -x -f ${_db}.colnames ${columns} | \
-cut -f1 > ${_db}.colid
-fn=$(cat ${_db}.colid)
-echo $fn > ${_db}.cat
-fields=$(sed 's/ /,/g' ${_db}.cat)
+zgrep -n -T -x -f ${db}.colnames ${columns} | \
+cut -f1 > ${db}.colid
+fn=$(cat ${db}.colid)
+echo $fn > ${db}.cat
+fields=$(sed 's/ /,/g' ${db}.cat)
 gunzip -c ${BP} | \
-cut -f1 > ${_db}.rownames
+cut -f1 > ${db}.rownames
 gunzip -c ${BP} | \
 cut -f1 --complement | \
 cut -f$fields | \
-paste ${_db}.rownames - > ${_db}.network
+paste ${db}.rownames - > ${db}.network
 
 ## vv slow!
-# gunzip -c ${BP} | awk -vFS="\t" -vOFS="\t" -f xpose.awk | grep -x -f ${_db}.colnames | awk -f xpose.awk > ${_db}.network
+# gunzip -c ${BP} | awk -vFS="\t" -vOFS="\t" -f xpose.awk | grep -x -f ${db}.colnames | awk -f xpose.awk > ${db}.network
 
 R --no-save -q < $PW_location/files/network.R > network.log
 
