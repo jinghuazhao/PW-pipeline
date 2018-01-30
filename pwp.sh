17e9052f32f43b7c8813df4c09a9f30f4d5fbbff
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-30 14:27:11 +0000

    main program

--- pwp.sh
+++ pwp.sh
@@ -7,7 +7,7 @@
 export mp=0
 export R_LIBS=/genetics/bin/R:/usr/local/lib64/R/library
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/genetics/data/software/lib
-export PATH=/genetics/bin/anaconda2/bin:$PATH:/genetics/bin:/usr/local/bin;/genetics/data/software/bin
+export PATH=/genetics/bin/anaconda2/bin:$PATH:/genetics/bin:/usr/local/bin:/genetics/data/software/bin
 export PYTHONPATH=/genetics/bin/anaconda2/lib/python2.7/site-packages
 
 export MAGENTA=/genetics/bin/MAGENTA_software_package_vs2_July2011
