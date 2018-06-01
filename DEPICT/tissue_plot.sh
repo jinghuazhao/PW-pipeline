faea853de84ab07a9c105de5ad625421a9170722
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-01 21:07:58 +0100

    auxiliary directories

--- DEPICT/tissue_plot.sh
+++ DEPICT/tissue_plot.sh
@@ -1,4 +1,4 @@
-# 5-4-2018 MRC-Epid JHZ
+# 1-6-2018 MRC-Epid JHZ
 
 # R CMD BATCH --no-save --no-restore '--args ${1}_tissueenrichment.txt' tissue_plot.R ${1}.out
 
@@ -12,7 +12,7 @@ do
    if [ -f ${r}.png ]; then
       rm ${r}.png
    fi
-   pdftopng ${r}.pdf -r 300 ${r}
+   pdftopng -r 300 ${r}.pdf ${r}
    if [ -f ${r}-000001.png ]; then
       mv ${r}-000001.png ${r}.png
    fi
