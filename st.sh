7a96766fa33ff394f7ad5e65eaa88ce29760142c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-07-28 10:01:18 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,17 @@
+# 28-7-2018 MRC-Epid JHZ
+
+git add .gitignore
+git commit -m "Don't process these"
+git add files
+git commit -m "auxiliary files"
+git add MAGENTA MAGMA PASCAL DEPICT
+git commit -m "auxiliary directories"
+git add pwp.sh
+git commit -m "main program"
+git add vegas2v2.sh
+git commit -m "Gene and pathway analysis with VEGAS2v2"
+git add README.*
+git commit -m "README"
+git add st.sh
+git commit -m "st.sh"
+git push
