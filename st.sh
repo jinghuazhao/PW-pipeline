91f5826047845254b3402d48c1e4a50ac058c44a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-16 09:39:28 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -2,16 +2,16 @@
 
 git add .gitignore
 git commit -m "ignored files"
-git add files
-git commit -m "auxiliary files"
-git add MAGENTA MAGMA PASCAL DEPICT
-git commit -m "auxiliary directories"
 git add pwp.ini
 git commit -m "settings"
 git add pwp.sh
 git commit -m "main program"
 git add vegas2v2.sh
 git commit -m "Gene and pathway analysis with VEGAS2v2"
+git add MAGENTA MAGMA PASCAL DEPICT
+git commit -m "auxiliary directories"
+git add files
+git commit -m "auxiliary files"
 git add README.*
 git commit -m "README"
 git add st.sh
