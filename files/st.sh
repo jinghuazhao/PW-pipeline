78ec58ddcada364e3c2c20a8cf754457687774b9
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-02 19:10:31 +0000

    auxiliary files and directories

---
+++ files/st.sh
@@ -0,0 +1,15 @@
+#/usr/bin/bash
+
+git add .gitignore
+git commit -m "ignored files"
+git add pwp.ini
+git commit -m "settings"
+git add pwp.sh
+git commit -m "main program"
+git add vegas2v2.sh
+git commit -m "Gene and pathway analysis with VEGAS2v2"
+git add files MAGENTA MAGMA PASCAL DEPICT
+git commit -m "auxiliary files and directories"
+git add README.*
+git commit -m "README"
+git push
